accae203fdb19e51585c87bf1c656b5100f5bd73
tescujuri <84600614+tescujuri@users.noreply.github.com>
2023-08-31 22:58:30 -0700

    Update clean_vormetric_splunk_out.py

--- clean_vormetric_splunk_out.py
+++ clean_vormetric_splunk_out.py
@@ -12,8 +12,10 @@ with open(INPUT_FILE, 'r') as data:
    lines = data.readlines()
    for line in lines:
       line = line.strip()
-      cleaned_data = line.replace('.wellsfargo.com', '').replace('.wellsfargo.net', '').replace('.infra', '')
-      cleaned.append(cleaned_data)
+      cleaned_data = line.replace('.wellsfargo.com', '').replace('.wellsfargo.net', '') \
+         .replace('.infra', '')
+      if cleaned_data not in cleaned:
+         cleaned.append(cleaned_data)
    for clean in cleaned:
       output += clean.strip('"') + "\n"
    data.close()
