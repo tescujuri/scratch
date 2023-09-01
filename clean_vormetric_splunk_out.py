66d393dd7a5a71c5a58654e0584403d7fc36dcb1
tescujuri <84600614+tescujuri@users.noreply.github.com>
2023-09-01 15:53:57 -0700

    clean_vormetric_splunk_out.py updated for bad output from splunk

--- clean_vormetric_splunk_out.py
+++ clean_vormetric_splunk_out.py
@@ -1,24 +1,30 @@
+import re
+
 INPUT_FILE = "./data.in"
 OUTPUT_FILE = "./data.out"
 cleaned = []
-output = ""   
+output = ""
 
-def write_file(data):
-   with open(OUTPUT_FILE, 'w') as data_out:
-      data_out.write(data)
-      data_out.close()
-      
-def clean_data(data):
-   return data.replace('.bilatu.com', '').replace('.bilatu.net', '').replace('.dev', '').strip()
-      
+def write_file(data_in):
+    with open(OUTPUT_FILE, 'w') as data_out:
+        data_out.write(data_in)
+        data_out.close()
+
+def clean_data(data_in):
+    prefix_clean = re.sub(r".*:", '', data_in)
+    prefix_clean1 = prefix_clean.split(" ")[0]
+    cleaned_out = prefix_clean.replace('.bilatu.com', '').replace('.bilatu.net', '').replace('.dev', '').replace('..', ".").replace('"', '').replace("^ ", "")
+    return cleaned_out
+
 with open(INPUT_FILE, 'r') as data:
-   lines = data.readlines()
-   for line in lines:
-      cleaned_data = clean_data(line)
-      if cleaned_data not in cleaned:
-         cleaned.append(cleaned_data)
-   for clean in cleaned:
-      output += clean.strip('"') + "\n"
-   data.close()
-   
+    lines = data.readlines()
+    for line in lines:
+        cleaned_data = clean_data(line)
+        if cleaned_data not in cleaned:
+            cleaned.append(cleaned_data)
+    for clean in cleaned:
+        output += clean.strip('"')
+
+    data.close()
+
 write_file(output)
